0640b6de66d648d0c69ad88b25ecd2d938b739d3
jsonpreziuso <89964776+jsonpreziuso@users.noreply.github.com>
2021-10-07 14:14:50 +0200

    Update lehrerLogin.js

--- js/lehrerLogin.js
+++ js/lehrerLogin.js
@@ -12,15 +12,19 @@ $(document).ready(function() {
       var email = $("#emailFeld").val();
       var passwort = $("#passwortFeld").val();
 
-      if(email == "" || passwort == ""){
+      if(email.trim() == "" || passwort.trim() == ""){
         alert("Bitte geben Sie Ihre E-Mail und Ihr Passwort ein");
-      }else if (passwort != "admin" /*HERE WE HAVE TO CHECK IF THE EMAIL EXISTS IN THE DATABASE, AND IF THE PASSWORD IS THE CORRECT PASSWORD FOR THE EMAIL*/) { 
-        window.location.href='lehrerVerwaltung.html'; //HERE WE HAVE TO DIRECT THE LEHRER TO THEIR OWN VERWALTUNG WITH THEIR OWN DETAILS ALREADY INSIDE
+      }else if (passwort != "admin" /*HERE WE HAVE TO CHECK IF THE EMAIL EXISTS IN THE DATABASE, AND IF THE PASSWORD IS THE CORRECT PASSWORD FOR THE EMAIL*/) {
+        window.location.href='lehrerKlausur.html'; //HERE WE HAVE TO DIRECT THE LEHRER TO THEIR OWN VERWALTUNG WITH THEIR OWN DETAILS ALREADY INSIDE
       }else if (passwort == "admin"){
+        //SQL QUERY HERE TO SEE IF USER WITH THIS EMAIL AND PASSWORD HAS ADMIN STATUS
+        //IF YES
         window.location.href='admin.html';
+        //IF NO
+        //alert("Zugriff verweigert, Sie sind kein Administrator")
       }else{
         alert("Ungültige Zugangsdaten");
       }
-      
+
     });
 });
