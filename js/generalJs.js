a37a87236fb2e3b727de884c47aaa15d0b57fa69
I3re2eL <44670825+I3re2eL@users.noreply.github.com>
2021-10-14 07:10:50 +0200

    Add files via upload

---
+++ js/generalJs.js
@@ -0,0 +1,7 @@
+function overlayOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+        
+function overlayOff() {
+    document.getElementById("overlay").style.display = "none";
+}
