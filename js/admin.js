061a2b289fc2c7bd04c1bc0793cf42384b40fd84
jsonpreziuso <89964776+jsonpreziuso@users.noreply.github.com>
2021-10-15 15:46:21 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -8,15 +8,40 @@ LF6 - Herr Grüning
 
 $(document).ready(function() {
 
-//AUTOFILL FIELDS WILL BE IMPLEMENTED WHEN SELECTING A USER FROM THE TABLE ONCE THE TABLE IS IMPLEMENTED
+//UPLOAD FILE FUNCTION WILL BE IMPLEMENTED ONCE DATABASE IS IMPLEMENTED
 
-//SEARCH FUNCTION WILL BE IMPLEMENTED ONCE TABLE IS IMPLEMENTED
+//DATATABLE MANAGEMENT
+  var adminTabelle = $('#adminTabelle').DataTable();
+  var lehrerSelected = false;
 
-//UPLOAD FILE FUNCTION WILL BE IMPLEMENTED ONCE TABLE IS IMPLEMENTED
+  //HERE ALL TABLE DATA HAS TO BE TAKEN FROM THE DATABASE AND INSERTED INTO THE
+  //TABLE DYNAMICALLY. EACH DYNAMICALLY CREATED <TR> NEEDS TO BE GIVEN AN "ID" WHICH
+  //SHOULD BE EQUAL TO THE DATABASE ROW ID. WITHIN EACH <TR>, EVERY <TD> HAS TO BE
+  //GIVEN AN "ID" OF "adminCheckRow, vorNameRow, nachNameRow, emailRow"
+  //RESPECTIVELY, WITH IT'S PARENT <TR> ID BEING ADDED TO THE END OF EACH ONE
+  //E.G. IF <TR id="5">, THEN ALL <TD>s SHOULD HAVE "5" ADDED TO THE END
+  //<TR id="5"><TD id="adminCheckRow5"></TD>, <TD id="vorNameRow5"></TD> ... etc </TR>
 
-//SETTING NEW ADMINS WILL BE IMPLEMENTED ONCE TABLE IS IMPLEMENTED
+  $('#adminTabelle td').click(function(){
 
-//ANDEARN BUTTON
+    lehrerSelected = true;
+    $('#adminTabelle tr').css("background-color","");
+    $('#adminTabelle tr').removeAttr("selected");
+    $(this).parent().css("background-color","#a5dee8");
+    $(this).parent().attr("selected","true");
+
+/*
+  //  ALL THIS WILL ONLY WORK ONCE TABLE IS GENERATED WITH DATABASE DATA
+
+    //AUTOFILL VALUES
+    var row_index = $(this).parent().attr("id");
+    $("#vornameFeld").val($("#vorNameRow"+row_index).text());
+    $("#nachnameFeld").val($("#nachNameRow"+row_index).text());
+    $("#emailFeld").val($("#emailRow"+row_index).text());
+  */
+  });
+
+//ANDEARN-FUNKTION
   $("#btnAendern").click(function() {
 
     //RESET BACKGROUND COLOURS BACK TO WHITE
@@ -24,66 +49,73 @@ $(document).ready(function() {
     $("#nachnameFeld").css("background-color","white");
     $("#emailFeld").css("background-color","white");
 
-    //SAVE ALL INPUTED DATA INTO VARIABLES
-    var vorname = $("#vornameFeld").val();
-    var nachname = $("#nachnameFeld").val();
-    var email = $("#emailFeld").val();
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-
-    //VALIDATE INPUTTED DATA
-    if(vorname.trim() == ""){
-      alert("Bitte geben Sie Ihre Vorname ein");
-      $("#vornameFeld").css("background-color","lightcoral");
-    }else if(nachname.trim() == ""){
-      alert("Bitte geben Sie Ihre Nachname ein");
-      $("#nachnameFeld").css("background-color","lightcoral");
-    }else if(email == "" || !emailReg.test(email)){
-      alert("Bitte geben Sie eine gültige Email-Adresse ein");
-      $("#emailFeld").css("background-color","lightcoral");
-    }else if(false/* SQL QUERY TO CHECK IF A USER WITH THE SAME LOGIN DATA ALREADY EXISTS IN THE DATABASE*/){
-      alert("Ihre ausgewählten Zugangsdaten sind nicht verfügbar");
+    if(!lehrerSelected){
+      alert("Bitte wählen Sie ein Lehrer/in aus");
     }else{
-      alert("Ihre Zugangsdaten wurden erfolgreich aktualisiert");
-      $("#vornameFeld").css("background-color","lightgreen");
-      $("#nachnameFeld").css("background-color","lightgreen");
-      $("#emailFeld").css("background-color","lightgreen");
-      //SQL QUERIES TO UPDATE SELECTED USER'S DATA WITH ALL NEWLY INPUTED DATA
-    }
-  });
+      if(confirm("Möchten Sie + GET LEHRERNAME + Konto-Daten wirklich ändern?")){
 
-//LOESCH BUTTON
-  $("#btnLoeschen").click(function() {
-    if(confirm("Möchten Sie " + /*GET VORNAME + NACHNAME FROM SELECTED USER+*/ "wirklich aus der Datenbank löschen?")){
+        //SAVE ALL INPUTED DATA INTO VARIABLES
+        var vorname = $("#vornameFeld").val();
+        var nachname = $("#nachnameFeld").val();
+        var email = $("#emailFeld").val();
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-      //var vorname = VORNAME FROM ENTRY SELECTED IN TABLE
-      //var nachname = NACHNAME FROM ENTRY SELECTED IN TABLE
-      //var email = EMAIL FROM ENTRY SELECTED IN TABLE
-
-      /* TABLE FUNCTION READY FOR WHEN TABLE HAS BEEN IMPLEMENTED IN HTML
-        var table = $('#example').DataTable();
-        if (!table.rows( '.selected' ).any()){
-          alert("Bitte wählen Sie eine zu löschende User aus");
+        //VALIDATE INPUTTED DATA
+        if(vorname.trim() == ""){
+          alert("Bitte geben Sie Ihre Vorname ein");
+          $("#vornameFeld").css("background-color","lightcoral");
+        }else if(nachname.trim() == ""){
+          alert("Bitte geben Sie Ihre Nachname ein");
+          $("#nachnameFeld").css("background-color","lightcoral");
+        }else if(email == "" || !emailReg.test(email)){
+          alert("Bitte geben Sie eine gültige Email-Adresse ein");
+          $("#emailFeld").css("background-color","lightcoral");
+        }else if(false /*SQL QUERY TO CHECK IF A USER WITH THE SAME LOGIN DATA ALREADY EXISTS IN THE DATABASE*/){
+          alert("Ihre ausgewählten Zugangsdaten sind nicht verfügbar");
         }else{
-          //ADD SQL QUERIES HERE TO DELETE THE SELECTED USER FROM THE DATABASE
-          alert(GET VORNAME + NACHNAME FROM SELECTED USER + " wurde erfolgreich aus der Datenbank gelöscht");
-          //RELOAD TABLE TO DISPLAY THE NEW ENTRIES
+          alert("Ihre Zugangsdaten wurden erfolgreich aktualisiert");
+          $("#vornameFeld").css("background-color","lightgreen");
+          $("#nachnameFeld").css("background-color","lightgreen");
+          $("#emailFeld").css("background-color","lightgreen");
+
+          //HERE ALL DATABASE VALUES HAVE TO BE UPDATED FOR THE SELECTED LEHRER
+          //WHERE THE <TR> ID (REPRESENTED BY "var row_index") MATCHES THE ROW ID IN THE DATABASE
         }
-      */
+      }
     }
   });
-//PASSWORTZURUECKSETZEN BUTTON
+
+
+//LOESCH-FUNKTION
+
+  $("#btnLoeschen").click(function() {
+
+    if(!lehrerSelected){
+      alert("Bitte wählen Sie ein Lehrer/in aus");
+    }else{
+      if(confirm("Möchten Sie " /*+ GET VORNAME + NACHNAME FROM SELECTED LEHRER */ + "wirklich aus der Datenbank löschen?")){
+
+        //IF YES THEN DELETE ENTIRE ROW FROM DATABASE WHERE THE <TR> ID (REPRESENTED BY "var row_index")
+        //MATCHES THE ROW ID IN THE DATABASE
+
+        alert(/*GET VORNAME + NACHNAME FROM SELECTED LEHRER */" wurde erfolgreich aus der Datenbank gelöscht");
+
+        //DATATABLE.RELOAD();
+      }
+    }
+  });
+
+
+//PASSWORTZURUECKSETZEN-FUNKTION
   $("#btnPasswortZuruecksetzen").click(function() {
-
-    if (confirm("Sind Sie sicher, dass Sie das Passwort von " + /*GET VORNAME + NACHNAME FROM SELECTED USER+*/ " auf die Standardeinstellungen zurücksetzen möchten?")) {
-
-      //var vorname = VORNAME FROM ENTRY SELECTED IN TABLE
-      //var nachname = NACHNAME FROM ENTRY SELECTED IN TABLE
-      //var email = EMAIL FROM ENTRY SELECTED IN TABLE
-
-      //ADD SQL QUERIES HERE TO FIND THE SELECTED USER IN THE DATABASE AND
-      //RESET HIS PASSWORD TO DEFAULT E.G.("ABC123")
-      //PASSWORD MUST BE HASHED BEFORE BEING SAVED IN THE DATABASE
-      alert(/*GET VORNAME + NACHNAME FROM SELECTED USER +*/ " Passwort wurde erfolgreich auf die Standardeinstellungen zurückgesetzt");
+    if(!lehrerSelected){
+      alert("Bitte wählen Sie ein Lehrer/in aus");
+    }else{
+      if(confirm("Sind Sie sicher, dass Sie das Passwort von " + /*GET VORNAME + NACHNAME FROM SELECTED LEHRER+ */" auf die Standardeinstellungen zurücksetzen möchten?")) {
+        //IF YES THEN SET PASSWORD TO "default123" WHERE THE <TR> ID (REPRESENTED BY "var row_index")
+        //MATCHES THE ROW ID IN THE DATABASE
+        alert(/*GET VORNAME + NACHNAME FROM SELECTED LEHRER + */" Passwort wurde erfolgreich auf die Standardeinstellungen zurückgesetzt");
+      }
     }
   });
 });
