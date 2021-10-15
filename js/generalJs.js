88c54527c751f257496b64ba0ad6a6cab820829c
jsonpreziuso <89964776+jsonpreziuso@users.noreply.github.com>
2021-10-15 15:46:38 +0200

    Update generalJs.js

--- js/generalJs.js
+++ js/generalJs.js
@@ -1,7 +1,18 @@
 function overlayOn() {
     document.getElementById("overlay").style.display = "block";
 }
-        
+
 function overlayOff() {
     document.getElementById("overlay").style.display = "none";
 }
+
+//TABS
+$("#btnMeinKonto").click(function() {
+  window.location.href='lehrerMeinKonto.html';
+});
+$("#btnKlasse").click(function() {
+  window.location.href='lehrerKlasse.html';
+});
+$("#btnKlausur").click(function() {
+  window.location.href='lehrerKlausur.html';
+});
