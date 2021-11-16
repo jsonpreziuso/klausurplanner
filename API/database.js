2acc822763a39abc8849915d15951c4d2d4492e5
don1910 <89966353+don1910@users.noreply.github.com>
2021-11-16 20:09:09 +0100

    Add files via upload
    Klausuren/Klassen:  hinzufügen, update, löschen Methoden hinzugefügt

--- API/database.js
+++ API/database.js
@@ -9,7 +9,7 @@ LF6 - Herr Grüning
 
 
 
-const mysql = require ('mysql');
+const mysql = require('mysql');
 const { resolve } = require('path/posix');
 
 const mysqlConnection = mysql.createConnection({
@@ -18,9 +18,9 @@ const mysqlConnection = mysql.createConnection({
     password: "TearTable2021",
     database: "tearproject",
     port: "3306"
-}); 
+});
 
-mysqlConnection.connect(function (err){
+mysqlConnection.connect(function (err) {
     if (err) {
         console.log(err);
     } else {
@@ -32,9 +32,9 @@ let db = {};
 
 //get all classes from DB
 db.getAllClasses = () => {
-    return new Promise ((resolve,reject) =>{
-        mysqlConnection.query('SELECT * FROM klassen', (error,classesList) =>{
-            if(error){
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('SELECT * FROM klassen', (error, classesList) => {
+            if (error) {
                 return reject(error);
             }
             return resolve(classesList);
@@ -44,20 +44,20 @@ db.getAllClasses = () => {
 
 //get an classe rowi in the table by the id
 db.getClasseById = (id) => {
-    return new Promise ((resolve,reject) =>{
-        mysqlConnection.query('SELECT idKlassen,name,passwort FROM klassen WHERE IdKlassen =?',id,(error,classe) =>{
-            if(error){
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('SELECT idKlassen,name,passwort FROM klassen WHERE IdKlassen =?', id, (error, classe) => {
+            if (error) {
                 return reject(error);
             }
-            return resolve (classe);
+            return resolve(classe);
         });
     })
 }
 
 //get all the exams from the DB
 db.getAllExams = () => {
-    return new Promise ((resolve,reject) =>{
-        mysqlConnection.query('SELECT * FROM klausuren', (error,examsList) =>{
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('SELECT * FROM klausuren', (error, examsList) => {
             if (error) {
                 return reject(error);
             }
@@ -68,19 +68,19 @@ db.getAllExams = () => {
 
 //get an exam row in the table by the id
 db.getExamById = (id) => {
-    return new Promise ((resolve,reject) =>{
-        mysqlConnection.query('SELECT idKlausuren,fk_lehrer,fk_klassen,fach,datum,schulestunde,raumnummer,thema FROM klausuren WHERE IdKlausuren =?',id,(error,exam) =>{
-            if(error){
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('SELECT idKlausuren,fk_lehrer,fk_klassen,fach,datum,schulestunde,raumnummer,thema FROM klausuren WHERE IdKlausuren =?', id, (error, exam) => {
+            if (error) {
                 return reject(error);
             }
-            return resolve (exam);
+            return resolve(exam);
         });
     })
 }
 
 db.getAllTeachers = () => {
-    return new Promise ((resolve,reject) =>{
-        mysqlConnection.query('SELECT * FROM lehrer', (error,TeachersList) =>{
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('SELECT * FROM lehrer', (error, TeachersList) => {
             if (error) {
                 return reject(error);
             }
@@ -91,50 +91,118 @@ db.getAllTeachers = () => {
 
 //get an exam row in the table by the id
 db.getTeacherById = (id) => {
-    return new Promise ((resolve,reject) =>{
-        mysqlConnection.query('SELECT idlehrer,admin,vorname,nachname,email,passwort FROM lehrer WHERE Idlehrer =?',id,(error,teacher) =>{
-            if(error){
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('SELECT idlehrer,admin,vorname,nachname,email,passwort FROM lehrer WHERE Idlehrer =?', id, (error, teacher) => {
+            if (error) {
                 return reject(error);
             }
-            return resolve (teacher);
+            return resolve(teacher);
         });
     })
 }
 
 db.insertTeacher = (body) => {
-    return new Promise ((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         mysqlConnection.query('INSERT INTO lehrer(admin,vorname,nachname,email,passwort) VALUES (?,?,?,?,?)',
-        [body.admin,body.vorname,body.nachname,body.email,body.passwort],(error,body) => {
-            if (error) {
-                return reject(error);
-            }
-           return resolve (body);
-        } );   
+            [body.admin, body.vorname, body.nachname, body.email, body.passwort], (error, body) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(body);
+            });
     })
 }
 
-db.updateTeacherById = (body,id) => {
-    return new Promise ((resolve,reject) => {
-        mysqlConnection.query('UPDATE lehrer SET admin= ?,vorname= ?,nachname=?,email=?,passwort=?  WHERE idlehrer =?',id,(error,teacher)
-        [body.admin,body.vorname,body.nachname,body.email,body.passwort,id],(error,body) => {
+db.updateTeacherById = (body, id) => {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('UPDATE lehrer SET admin= ?,vorname= ?,nachname=?,email=?,passwort=?  WHERE idlehrer =?', id, (error, teacher)
+        [body.admin, body.vorname, body.nachname, body.email, body.passwort, id], (error, body) => {
             if (error) {
                 return reject(error);
             }
-           return resolve (body); 
+            return resolve(body);
         })
     })
 }
 
 db.deleteTeacherById = (id) => {
-    return new Promise ((resolve,reject) => {
-        mysqlConnection.query('DELETE FROM lehrer WHERE idlehrer= ?',id,(error,teacher) =>{
-            if(error){
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('DELETE FROM lehrer WHERE idlehrer= ?', id, (error, teacher) => {
+            if (error) {
                 return reject(error);
             }
-            return resolve (teacher);
+            return resolve(teacher);
         })
     })
 }
 
+db.insertExam = (body) => {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('INSERT INTO klausuren(admin,fk_lehrer,fk_klassen,fach,datum,schulestunde,raumnummer,thema) VALUES(?,?,?,?,?,?,?,?)',
+            [body.admin, body.fk_lehrer, body.fk_klassen, body.fach, body.datum, body.schulestunde, body.raumnummer, body.thema], (error, body) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(body);
+            });
+    })
+}
+
+db.updateExamById = (body, id) => {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('UPDATE klausur SET admin= ?,fk_lehrer= ?,fk_klassen=?,fach=?,datum=,schulestunde=?,raumnummer=?,thema=?  WHERE idExam =?', id, (error, klausur)
+        [body.admin, body.fk_lehrer, body.fk_klassen, body.fach, body.datum, body.schulestunde, body.raumnummer, body.thema], (error, body) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(body);
+        })
+    })
+}
+
+db.deleteExamById = (id) => {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('DELETE FROM klausuren WHERE idExam= ?', id, (error, klausur) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(teacher);
+        })
+    })
+}
+db.insertClass = (body) => {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('INSERT INTO klassen(admin,name,passwort) VALUES (?,?,?)',
+            [body.admin, body.name, body.password], (error, body) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(body);
+            });
+    })
+}
+
+db.updateClassById = (body, id) => {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('UPDATE klassen SET admin= ?,name= ?,passwort=?  WHERE idklassen =?', id, (error, klassen)
+        [body.admin, body.name, body.password], (error, body) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(body);
+        })
+    })
+}
+
+db.deleteClassbyId = (id) => {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query('DELETE FROM klassen WHERE idklassen= ?', id, (error, klassen) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(klassen);
+        })
+    })
+}
 module.exports = mysqlConnection;
 module.exports = db;
--- API/index.js
+++ API/index.js
@@ -7,41 +7,41 @@ LF6 - Herr Grüning
 */
 
 
-const express = require ('express');
+const express = require('express');
 const app = express();
 
 
 const mysqlConnection = require('../API/database');
-const db = require ('../API/database');
+const db = require('../API/database');
 
 app.use(express.json());
 
 const exams = [
-    { id: 1, lehrerId:1, KlasseId :1, fach :'LF2', Schulestunde:2, raumnummer:201,thema:'Struktogram' },
-    { id: 2, lehrerId:3, KlasseId :1, fach :'LF5', Schulestunde :1, raumnummer:201, thema:'Simple present' },
-    { id: 3, lehrerId:2, KlasseId :1, fach :'LF6' , Schulestunde :5, raumnummer :201, thema :'UML' },
-    { id: 4, lehrerId:4, KlasseId :1, fach :'LF9' , Schulestunde :4, raumnummer :201, thema :'IP6 & Offene netz' },
+    { id: 1, lehrerId: 1, KlasseId: 1, fach: 'LF2', Schulestunde: 2, raumnummer: 201, thema: 'Struktogram' },
+    { id: 2, lehrerId: 3, KlasseId: 1, fach: 'LF5', Schulestunde: 1, raumnummer: 201, thema: 'Simple present' },
+    { id: 3, lehrerId: 2, KlasseId: 1, fach: 'LF6', Schulestunde: 5, raumnummer: 201, thema: 'UML' },
+    { id: 4, lehrerId: 4, KlasseId: 1, fach: 'LF9', Schulestunde: 4, raumnummer: 201, thema: 'IP6 & Offene netz' },
 ]
 
 
 //get all the school classes
-app.get('/api/classes', async (req, res, next)=>{
+app.get('/api/classes', async (req, res, next) => {
     try {
         const classes = await db.getAllClasses();
         res.status(200).json(classes);
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         res.sendStatus(500);
     }
 });
 
 //get a classe by a id given
-app.get('/api/classes/:id' ,async (req,res, next) =>{
+app.get('/api/classes/:id', async (req, res, next) => {
     try {
         var id = req.params.id;
         const classe = await db.getClasseById(id);
         res.status(200).json(classe);
-        
+
     } catch (e) {
         console.log(e);
         res.sendStatus(500);
@@ -49,11 +49,11 @@ app.get('/api/classes/:id' ,async (req,res, next) =>{
 })
 
 // get the complete list of exams
-app.get('/api/exams', async (req, res, next)=>{
+app.get('/api/exams', async (req, res, next) => {
     try {
         const exams = await db.getAllExams();
         res.status(200).json(exams);
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         res.sendStatus(500);
     }
@@ -61,12 +61,12 @@ app.get('/api/exams', async (req, res, next)=>{
 
 
 // get an exam by the id
-app.get('/api/exams/:id' ,async (req,res, next) =>{
+app.get('/api/exams/:id', async (req, res, next) => {
     try {
         var id = req.params.id;
         const exam = await db.getExamById(id);
         res.status(200).json(exam);
-        
+
     } catch (e) {
         console.log(e);
         res.sendStatus(500);
@@ -75,11 +75,11 @@ app.get('/api/exams/:id' ,async (req,res, next) =>{
 
 
 // get the complete list of teachers
-app.get('/api/teachers', async (req, res, next)=>{
+app.get('/api/teachers', async (req, res, next) => {
     try {
         const teachers = await db.getAllTeachers();
         res.status(200).json(teachers);
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         res.sendStatus(500);
     }
@@ -87,12 +87,12 @@ app.get('/api/teachers', async (req, res, next)=>{
 
 
 // get a teacher by the id
-app.get('/api/teachers/:id' ,async (req,res, next) =>{
+app.get('/api/teachers/:id', async (req, res, next) => {
     try {
         var id = req.params.id;
         const teacher = await db.getTeacherById(id);
         res.status(200).json(teacher);
-        
+
     } catch (e) {
         console.log(e);
         res.sendStatus(500);
@@ -100,7 +100,7 @@ app.get('/api/teachers/:id' ,async (req,res, next) =>{
 })
 
 // Create & insert new teacher in teacher's table 
-app.post('/api/teachers/', async (req,res,next) =>{
+app.post('/api/teachers/', async (req, res, next) => {
     try {
         var body = req.body;
         console.log('body', body)
@@ -114,26 +114,26 @@ app.post('/api/teachers/', async (req,res,next) =>{
 
 
 // update teacher's info for a given id 
-app.put('/api/teachers/:id',async (req,res) =>{
+app.put('/api/teachers/:id', async (req, res) => {
     try {
         var body = req.body;
         var id = req.params.id;
-        console.log('body',body);
-        console.log('id',id);
-        var teacher = await db.updateTeacherById(body,id);
+        console.log('body', body);
+        console.log('id', id);
+        var teacher = await db.updateTeacherById(body, id);
         res.sendStatus(204);
     } catch (e) {
-        console.log(e); 
-        res.sendStatus(400);  
+        console.log(e);
+        res.sendStatus(400);
     }
 })
 
 // delete teacher's info for a given id
-app.delete('/api/teachers/:id',async (req,res) =>{
+app.delete('/api/teachers/:id', async (req, res) => {
     try {
         var id = req.params.id;
         //console.log('body',body);
-        console.log('id',id);
+        console.log('id', id);
         var teacher = await db.deleteTeacherById(id);
         res.sendStatus(204);
     } catch (e) {
@@ -142,6 +142,88 @@ app.delete('/api/teachers/:id',async (req,res) =>{
     }
 })
 
+// Create & insert new exams in exams table 
+app.post('/api/exams', async (req, res, next) => {
+    try {
+        var body = req.body;
+        console.log('body', body)
+        var teacher = await db.insertExam(body);
+        res.status(204).json(teacher);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
+// update exams info for a given id 
+app.put('/api/exams/:id', async (req, res) => {
+    try {
+        var body = req.body;
+        var id = req.params.id;
+        console.log('body', body);
+        console.log('id', id);
+        var teacher = await db.updateExamById(body, id);
+        res.sendStatus(204);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
+// delete exams info for a given id
+app.delete('/api/exams/:id', async (req, res) => {
+    try {
+        var id = req.params.id;
+        //console.log('body',body);
+        console.log('id', id);
+        var teacher = await db.deleteExamById(id);
+        res.sendStatus(204);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
+// Create & insert new classes in klassen table 
+app.post('/api/klassen', async (req, res, next) => {
+    try {
+        var body = req.body;
+        console.log('body', body)
+        var teacher = await db.insertClass(body);
+        res.status(204).json(klassen);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
+// update classes info for a given id 
+app.put('/api/klassen/:id', async (req, res) => {
+    try {
+        var body = req.body;
+        var id = req.params.id;
+        console.log('body', body);
+        console.log('id', id);
+        var teacher = await db.updateClassById(body, id);
+        res.sendStatus(204);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+// delete classes info for a given id
+app.delete('/api/klassen/:id', async (req, res) => {
+    try {
+        var id = req.params.id;
+        //console.log('body',body);
+        console.log('id', id);
+        var teacher = await db.deleteClassbyId(id);
+        res.sendStatus(204);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
 
 /*
 app.post('/api/teachers',async (req,res,next) =>{
@@ -172,8 +254,8 @@ app.post('/api/teachers',async (req,res,next) =>{
 
 
 
-app.get('/', (req, res)=>{
-     res.send('Hello world');
+app.get('/', (req, res) => {
+    res.send('Hello world');
 });
 
 /*
