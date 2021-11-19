3098c182d5f26d4cbbcceec67d4620747ecdb25b
jsonpreziuso <89964776+jsonpreziuso@users.noreply.github.com>
2021-11-19 15:03:39 +0100

    Update lehrerKlasse.js

--- js/lehrerKlasse.js
+++ js/lehrerKlasse.js
@@ -5,82 +5,131 @@ Jason Preziuso
 Klausurplanner Projekt
 LF6 - Herr Grüning
 */
-$(document).ready(function() {
-  //INSERTION OF LEHRERNAME
-  //$("#lehrerName").val() == USE BACKEND SESSION VARIABLE TO GET THE LEHRERNAME AND SET IT HERE
 
-  //SET LEHRER NAME IN THE NAVBAR
-  //HERE THE SIGNED IN LEHRER'S NAME MUST BE TAKEN FROM DATABASE AND SET IN $("#lehrerTitelName")
-  $("#lehrerTitelName").text("TearTable - " + "LEHRER NAME")
+$(document).ready(function() {
+  //INSERTION OF LEHRERNAME IN HEADER
+  $("#lehrerTitelName").text("TearTable - " + localStorage.getItem('lehrerVorname') + " " + localStorage.getItem('lehrerNachname'));
 
   //LOGOUT FUNCTIONALITY
   $("#logout").click(function(){
-    //HERE WE NEED TO END THE SESSION VARIABLE ON THE BACKEND
+    localStorage.setItem('lehrerVorname',"");
+    localStorage.setItem('lehrerNachname',"");
+    localStorage.setItem('lehrerEmail',"");
+    localStorage.setItem('lehrerId',"");
+    localStorage.setItem('lehrerAdmin',"");
   });
 
-//KLASSE VERWALTUNG SEITE
+  classData = {};
+  //AUTOFILL DATATABLE
+  $.get( "http://localhost:8080/api/classes", function( data ) {
+    classData = data;
+    $('#lehrerKlasseTabelle').DataTable({
+          "processing" : true,
+          "ajax" : {
+              "url" : "http://localhost:8080/api/classes",
+              dataSrc : ''
+          },
+          "columns" : [ {
+              "data" : "idklassen"
+          },{
+              "data" : "name"
+          },{
+              "data" : "passwort"
+          }]
+    });
+  });
 
-  //DATATABLE MANAGEMENT
-  var lehrerKlasseTabelle = $('#lehrerKlasseTabelle').DataTable();
+  //TABLE HIGHLIGHT ON SELECT
   var klasseSelected = false;
-
-  //HERE ALL TABLE DATA HAS TO BE TAKEN FROM THE DATABASE AND INSERTED INTO THE
-  //TABLE DYNAMICALLY. EACH DYNAMICALLY CREATED <TR> NEEDS TO BE GIVEN AN "ID" WHICH
-  //SHOULD BE EQUAL TO THE DATABASE ROW ID. WITHIN EACH <TR>, EVERY <TD> HAS TO BE
-  //GIVEN AN "ID" OF "klasseVerwaltungRow, klassePasswortVerwaltungRow"
-  //RESPECTIVELY, WITH IT'S PARENT <TR> ID BEING ADDED TO THE END OF EACH ONE
-  //E.G. IF <TR id="5">, THEN ALL <TD>s SHOULD HAVE "5" ADDED TO THE END
-  //<TR id="5"><TD id="klasseVerwaltungRow5"></TD>, <TD id="klassePasswortVerwaltungRow5"></TD></TR>
-
-  $('#lehrerKlasseTabelle td').click(function(){
-
+  $('#lehrerKlasseTabelle').on("click","td",function(){
     klasseSelected = true;
     $('#lehrerKlasseTabelle tr').css("background-color","");
-    $('#lehrerKlasseTabelle tr').removeAttr("selected");
+    $('#lehrerKlasseTabelle tr').removeAttr("class");
     $(this).parent().css("background-color","#a5dee8");
-    $(this).parent().attr("selected","true");
+    $(this).parent().attr("class","selectedTrue");
   });
 
   //KLASSE-EINTRAGEN-FUNKTION
   $("#btnEintragen").click(function() {
 
-    //RESET BACKGROUND COLOURS BACK TO WHITE
+    //VISUAL COLOUR FEEDBACK
     $("#klasseFeld").css("background-color","white");
     $("#passwortFeld").css("background-color","white");
 
+    //CAPTURE USER INPUT
     var klasse = $("#klasseFeld").val();
     var passwort = $("#passwortFeld").val();
+    var duplicateData = false;
 
+    //CHECK FOR DUPLICATE ENTRIES
+    for(i of classData){
+      if(klasse.trim() == i.name)
+      duplicateData = true;
+    }
+
+    //VALIDATE INPUTTED DATA
     if(klasse.trim() == ""){
       alert("Bitte geben Sie einen Klassennamen ein");
       $("#klasseFeld").css("background-color","lightcoral");
     }else if(passwort.trim() == ""){
       alert("Bitte geben Sie ein Passwort ein");
       $("#passwortFeld").css("background-color","lightcoral");
-    }else if(false/*ADD SQL QUERIES HERE TO CHECK IF A CLASS WITH THIS NAME ALREADY EXISTS IN THE DATABASE*/){
+
+      //CHECK FOR DUPLICATE ENTRIES
+    }else if(duplicateData){
       alert("Eine Klasse mit diesem Namen existiert bereits");
+      $("#klasseFeld").css("background-color","lightcoral");
     }else{
       //ADD THE NEW CLASS AND PASSWORD TO THE DATABASE
-      $("#klasseFeld").css("background-color","lightgreen");
-      $("#passwortFeld").css("background-color","lightgreen");
+      $.ajax({
+        type: "POST",
+        dataType:'json',
+        url: "http://localhost:8080/api/classes",
+        data: {"name": $("#klasseFeld").val(), "passwort": $("#passwortFeld").val()}
+      });
       alert("Ihre neue Klasse wurde erfolgreich erstellt");
-      //DATATABLE.RELOAD();
+      location.reload();
     }
   });
 
   //KLASSE-LOESCHEN-FUNKTION
   $("#btnLoeschen").click(function() {
 
+    //MAKE SURE A CLASS IS SELECTED
     if (!klasseSelected){
       alert("Bitte wählen Sie eine zu löschende Klasse aus");
-    }else{
-      if(confirm("Möchten Sie + GET KLASSENNAME + wirklich aus der Datenbank löschen?")){
 
-        //IF YES THEN DELETE ENTIRE ROW FROM DATABASE WHERE WHERE THE <TR> ID (REPRESENTED BY "var row_index")
-        //MATCHES THE ROW ID IN THE DATABASE
-        alert("Ihre Klasse wurde erfolgreich gelöscht");
-        //DATATABLE.RELOAD();
-      }
+    }else{
+      //CHECK IF EXAMS STILL EXIST FOR CLASS
+      var noExams = true;
+      $.get("http://localhost:8080/api/exams", function( data ) {
+        for(a of data){
+          if($(".selectedTrue").children().siblings(":first").siblings(":first").text() == a.fk_klassen){
+            alert("Klasse kann nicht gelöscht werden, Klausuren für diese Klasse sind noch vorhanden")
+            noExams = false;
+            break;
+          }
+        }
+        if(noExams){
+        //DELETE CLASS FROM DATABASE
+          $.get("http://localhost:8080/api/classes", function( data ) {
+              for(i of data){
+                if($(".selectedTrue").children().siblings(":first").siblings(":first").text() == i.idklassen){
+                  if(confirm("Möchten Sie " + i.name + " wirklich aus der Datenbank löschen?")){
+                    $.ajax({
+                      type: "POST",
+                      dataType:'json',
+                      url: "http://localhost:8080/api/classes/:id",
+                      data: {"id": i.idklassen}
+                    });
+                    alert("Ihre Klasse wurde erfolgreich gelöscht");
+                    location.reload();
+                  }
+                }
+              }
+          });
+        }
+      });
     }
   });
 });
