bd5c807413dd06dda946056f638229be939b3255
jsonpreziuso <89964776+jsonpreziuso@users.noreply.github.com>
2021-11-19 15:03:03 +0100

    Update lehrerLogin.js

--- js/lehrerLogin.js
+++ js/lehrerLogin.js
@@ -6,25 +6,52 @@ Klausurplanner Projekt
 LF6 - Herr Grüning
 */
 $(document).ready(function() {
+  //GET LOGIN INFO FROM DATABASE
+  $.get("http://localhost:8080/api/teachers", function( data ) {
 
     $("#btnLogin").click(function() {
 
+      //SAVE USER INPUT
       var email = $("#emailFeld").val();
       var passwort = $("#passwortFeld").val();
+      var correctLogin = false;
+      var adminAccess = false;
 
+      //SAVE ALL USER DATA INTO LOCAL STORAGE
+      //NORMAL LOGIN
+      for(i of data){
+        if(email == i.email && passwort == i.passwort){
+          correctLogin = true;
+          localStorage.setItem('lehrerVorname', i.vorname);
+          localStorage.setItem('lehrerNachname', i.nachname);
+          localStorage.setItem('lehrerEmail', i.email);
+          localStorage.setItem('lehrerId', i.idlehrer);
+          localStorage.setItem('lehrerAdmin', i.admin);
+
+        //ADMIN LOGIN
+        }else if(email == i.email && passwort == "admin"){
+          if(i.admin == "True"){
+            adminAccess = true;
+            //SAVE ALL USER DATA INTO LOCAL STORAGE
+            localStorage.setItem('lehrerVorname', i.vorname);
+            localStorage.setItem('lehrerNachname', i.nachname);
+            localStorage.setItem('lehrerEmail', i.email);
+            localStorage.setItem('lehrerId', i.idlehrer);
+            localStorage.setItem('lehrerAdmin', i.admin);
+          }
+        }
+      }
+
+      //VALIDATE USER INPUT
       if(email.trim() == "" || passwort.trim() == ""){
         alert("Bitte geben Sie Ihre E-Mail und Ihr Passwort ein");
-      }else if (passwort != "admin" /*HERE WE HAVE TO CHECK IF THE EMAIL EXISTS IN THE DATABASE, AND IF THE PASSWORD IS THE CORRECT PASSWORD FOR THE EMAIL*/) {
-        window.location.href='lehrerKlausur.html'; //HERE WE HAVE TO DIRECT THE LEHRER TO THEIR OWN VERWALTUNG WITH THEIR OWN DETAILS ALREADY INSIDE
-      }else if (passwort == "admin"){
-        //SQL QUERY HERE TO SEE IF USER WITH THIS EMAIL AND PASSWORD HAS ADMIN STATUS
-        //IF YES
+      }else if(correctLogin) {
+        window.location.href='lehrerKlausur.html';
+      }else if(adminAccess){
         window.location.href='admin.html';
-        //IF NO
-        //alert("Zugriff verweigert, Sie sind kein Administrator")
       }else{
         alert("Ungültige Zugangsdaten");
       }
-
     });
+  });
 });
