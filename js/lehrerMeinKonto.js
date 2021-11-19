65834b5997aed30d2333e702f28843acdf980b99
jsonpreziuso <89964776+jsonpreziuso@users.noreply.github.com>
2021-11-19 15:02:48 +0100

    Update lehrerMeinKonto.js

--- js/lehrerMeinKonto.js
+++ js/lehrerMeinKonto.js
@@ -6,19 +6,25 @@ Klausurplanner Projekt
 LF6 - Herr Grüning
 */
 $(document).ready(function() {
-  //INSERTION OF LEHRERNAME
-  //$("#lehrerName").val() == USE BACKEND SESSION VARIABLE TO GET THE LEHRERNAME AND SET IT HERE
 
-  //SET LEHRER NAME IN THE NAVBAR
-  //HERE THE SIGNED IN LEHRER'S NAME MUST BE TAKEN FROM DATABASE AND SET IN $("#lehrerTitelName")
-  $("#lehrerTitelName").text("TearTable - " + "LEHRER NAME")
+  //INSERTION OF LEHRERNAME IN HEADER
+  $("#lehrerTitelName").text("TearTable - " + localStorage.getItem('lehrerVorname') + " " + localStorage.getItem('lehrerNachname'));
 
   //LOGOUT FUNCTIONALITY
   $("#logout").click(function(){
-    //HERE WE NEED TO END THE SESSION VARIABLE ON THE BACKEND
+    localStorage.setItem('lehrerVorname',"");
+    localStorage.setItem('lehrerNachname',"");
+    localStorage.setItem('lehrerEmail',"");
+    localStorage.setItem('lehrerId',"");
+    localStorage.setItem('lehrerAdmin',"");
   });
 
-//MEIN-KONTO VERWALTUNG SEITE
+  //AUTO FILL FIELDS WITH LEHRER DATA
+  $("#vornameFeld").val(localStorage.getItem('lehrerVorname'));
+  $("#nachnameFeld").val(localStorage.getItem('lehrerNachname'));
+  $("#emailFeld").val(localStorage.getItem('lehrerEmail'));
+
+  //MEIN-KONTO VERWALTUNG SEITE
   $("#btnChange").click(function() {
 
     //RESET BACKGROUND COLOURS BACK TO WHITE
@@ -35,34 +41,54 @@ $(document).ready(function() {
     var passwort = $("#passwortFeld").val();
     var wiederholenPasswort = $("#wiederholenPasswortFeld").val();
     var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    var duplicateData = false;
 
     //VALIDATE INPUTTED DATA
-    if(vorname.trim() == ""){
-      alert("Bitte geben Sie Ihre Vorname ein");
-      $("#vornameFeld").css("background-color","lightcoral");
-    }else if(nachname.trim() == ""){
-      alert("Bitte geben Sie Ihre Nachname ein");
-      $("#nachnameFeld").css("background-color","lightcoral");
-    }else if(email == "" || !emailReg.test(email)){
-      alert("Bitte geben Sie eine gültige Email-Adresse ein");
-      $("#emailFeld").css("background-color","lightcoral");
-    }else if(passwort.trim() == "" || passwort.trim() == "admin"){
-      alert("Ungültiges Passwort");
-      $("#passwortFeld").css("background-color","lightcoral");
-    }else if(wiederholenPasswort != passwort){
-      alert("Ihre Passwörter stimmen nicht überein");
-      $("#passwortFeld").css("background-color","lightcoral");
-      $("#wiederholenPasswortFeld").css("background-color","lightcoral");
-    }else if(false/* SQL QUERY TO CHECK IF A USER WITH THE SAME LOGIN DATA ALREADY EXISTS IN THE DATABASE*/){
-      alert("Ihre ausgewählten Zugangsdaten sind nicht verfügbar");
-    }else{
-      alert("Ihre Zugangsdaten wurden erfolgreich aktualisiert");
-      $("#vornameFeld").css("background-color","lightgreen");
-      $("#nachnameFeld").css("background-color","lightgreen");
-      $("#emailFeld").css("background-color","lightgreen");
-      $("#passwortFeld").css("background-color","lightgreen");
-      $("#wiederholenPasswortFeld").css("background-color","lightgreen");
-      //HERE UPDATE THE DATABASE WITH THE NEWLY ENTERED DATA
-    }
+    $.get("http://localhost:8080/api/teachers", function( data ) {
+      for(i of data){
+        if(vorname.trim() == i.vorname && nachname.trim() == i.nachname && email == i.email && passwort == i.passwort)
+        var duplicateData = true;
+      }
+
+      //VALIDATE INPUTTED DATA
+      if(vorname.trim() == ""){
+        alert("Bitte geben Sie Ihre Vorname ein");
+        $("#vornameFeld").css("background-color","lightcoral");
+      }else if(nachname.trim() == ""){
+        alert("Bitte geben Sie Ihre Nachname ein");
+        $("#nachnameFeld").css("background-color","lightcoral");
+      }else if(email == "" || !emailReg.test(email)){
+        alert("Bitte geben Sie eine gültige Email-Adresse ein");
+        $("#emailFeld").css("background-color","lightcoral");
+      }else if(passwort.trim() == ""){
+        alert("Ungültiges Passwort");
+        $("#passwortFeld").css("background-color","lightcoral");
+      }else if(wiederholenPasswort != passwort){
+        alert("Ihre Passwörter stimmen nicht überein");
+        $("#passwortFeld").css("background-color","lightcoral");
+        $("#wiederholenPasswortFeld").css("background-color","lightcoral");
+
+        //CHECK FOR DUPLICATE ENTRIES
+      }else if(duplicateData){
+        alert("Die ausgewählten Kontodaten sind bereits vorhanden");
+
+        //UPDATE ACCOUNT DATA
+      }else{
+        $.ajax({
+          type: "PUT",
+          dataType:'json',
+          url: "http://localhost:8080/api/teachers",
+          data: {"action": "lehrerMeinKonto", "idlehrer" : localStorage.getItem('lehrerId'), "vorname": vorname.trim(), "nachname": nachname.trim(), "email": email, "passwort": passwort.trim()}
+        });
+
+        //UPDATE LOCALSTORAGE VARIABLES
+        localStorage.setItem('lehrerVorname',vorname.trim());
+        localStorage.setItem('lehrerNachname',nachname.trim());
+        localStorage.setItem('lehrerEmail',email);
+
+        alert("Ihre Zugangsdaten wurden erfolgreich aktualisiert");
+        $("#lehrerTitelName").text("TearTable - " + localStorage.getItem('lehrerVorname') + " " + localStorage.getItem('lehrerNachname'));
+      }
+    });
   });
 });
