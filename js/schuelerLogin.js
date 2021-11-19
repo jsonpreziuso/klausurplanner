a9afb38ca439fdd88ce514db32d2e9b0fd7a6b17
jsonpreziuso <89964776+jsonpreziuso@users.noreply.github.com>
2021-11-19 15:02:14 +0100

    Update schuelerLogin.js

--- js/schuelerLogin.js
+++ js/schuelerLogin.js
@@ -6,18 +6,33 @@ Klausurplanner Projekt
 LF6 - Herr Grüning
 */
 $(document).ready(function() {
+  //GET LOGIN INFO FROM CLASSES
+  $.get("http://localhost:8080/api/classes", function( data ) {
 
+    //LOGIN FUNCTION
     $("#btnLogin").click(function() {
 
+      //SAVE USER INPUT
       var klasse = $("#klasseFeld").val();
       var passwort = $("#passwortFeld").val();
+      var correctLogin = false;
 
+      //VALIDATE USER INPUT
+      for(i of data){
+        if(klasse == i.name && passwort == i.passwort){
+          correctLogin = true;
+          localStorage.setItem('klasse', klasse);
+        }
+      }
+
+      //VALIDATE USER INPUT
       if(klasse.trim() == "" || passwort.trim() == ""){
         alert("Bitte geben Sie den Klassennamen und das Passwort ein");
-      }else if (true) {  //DATABANK QUERIES HERE
+      }else if (correctLogin) {
         window.location.href='klausurTermine.html';
       }else{
         alert("Ungültige Zugangsdaten");
       }
     });
+  });
 });
